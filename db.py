f3f6b06e649c98532ee3b8dc4d4696470c25dda4
BoYanZh <bomingzh@sjtu.edu.cn>
2021-04-13 12:01:26 +0800

    feat: logging

--- db.py
+++ db.py
@@ -1,13 +1,21 @@
+import logging
 from functools import lru_cache
 
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
 from config import settings
 
+logging.basicConfig(
+    handlers=[logging.FileHandler("mq.log"), logging.StreamHandler()],
+    format="%(asctime)s %(levelname)-8s: %(filename)s %(funcName)s %(lineno)s %(message)s",
+    datefmt="%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
+
 
 @lru_cache()
 def get_db() -> AsyncIOMotorDatabase:
-    print("Starting MongoDB connection.")
+    logging.info("Starting MongoDB connection.")
     client = AsyncIOMotorClient(settings.db_host, settings.db_port)
     db = client.get_database(settings.db_name)
     return db
--- rpc.py
+++ rpc.py
@@ -1,4 +1,13 @@
+import logging
 from typing import Any
+
+logging.basicConfig(
+    handlers=[logging.FileHandler("mq.log"), logging.StreamHandler()],
+    format="%(asctime)s %(levelname)-8s: %(filename)s %(funcName)s %(lineno)s %(message)s",
+    datefmt="%m-%d %H:%M:%S",
+    level=logging.INFO,
+)
+
 
 import pika
 from pika.adapters.blocking_connection import BlockingChannel
@@ -14,10 +23,14 @@ channel: BlockingChannel = connection.channel()
 channel.queue_declare(queue="rpc_queue")
 
 
-def on_request(ch: BlockingChannel, method: Any, props: Any, methodName: str) -> None:
-    print(" [.] get_data(%s)" % methodName)
-    response = get_data(methodName)
-
+def on_request(ch: BlockingChannel, method: Any, props: Any, body: bytes) -> None:
+    methodName = str(body)
+    logging.info(" [.] get_data(%s)" % methodName)
+    try:
+        response = get_data(methodName)
+    except ValueError:
+        response = "ValueError"
+    logging.info(" [>] response = %s" % response)
     ch.basic_publish(
         exchange="",
         routing_key=props.reply_to,
@@ -27,8 +40,9 @@ def on_request(ch: BlockingChannel, method: Any, props: Any, methodName: str) ->
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-channel.basic_qos(prefetch_count=1)
-channel.basic_consume(queue="rpc_queue", on_message_callback=on_request)
+if __name__ == "__main__":
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(queue="rpc_queue", on_message_callback=on_request)
 
-print(" [x] Awaiting RPC requests")
-channel.start_consuming()
+    logging.info(" [x] Awaiting RPC requests")
+    channel.start_consuming()
