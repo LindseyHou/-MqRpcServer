7990da4e40e6195c02c5a351ba707aa64212403a
lrq619 <lrq619@sjtu.edu.cn>
2021-05-15 15:04:56 +0800

    [feat]change the variable names in real_val.py

--- real_var.py
+++ real_var.py
@@ -1,24 +1,32 @@
-from db import get_col
-import pymongo
-from datetime import datetime
 import logging
+from datetime import datetime
+from typing import Any
 
-def getScores(companyID: str):
+import pymongo
+
+from db import get_col
+
+
+def getScores(companyID: str) -> Any:
     companyID_int: int = int(companyID[7:])
     score_col = get_col("score")
-    doc = score_col.find({"companyID": companyID_int}).sort({"time",pymongo.DESCENDING}).limit(1)
+    doc = (
+        score_col.find({"companyID": companyID_int})
+        .sort({"time", pymongo.DESCENDING})
+        .limit(1)
+    )
     logging.info("find the latest doc in time: " + doc["time"].strftime(""))
     res = {
-        "wellRateWhole":doc["wellRateWhole"],
+        "wellRateWhole": doc["wellRateWhole"],
+        "wellRateType": doc["wellRateType"],
         "safetyScore": doc["saftyScore"],
         "priorRect": doc["priorRect"],
-        "tempFireFacilityNameList": doc["tempFireFacilityNameList"],
-        "tempErrorFacilityNameList": doc["tempErrorFacilityNameList"],
-        "errorFacilities": doc["errorFacilities"],
+        "firePartCode": doc["tempFireFacilityNameList"],
+        "errorPartCode": doc["tempErrorFacilityNameList"],
+        "errorPartCodeMonth": doc["errorFacilities"],
         "detailScore": doc["detailScore"],
-        "errorRankType":doc["errorRankType"],
+        "errorRankType": doc["errorRankType"],
         "errorRankNum": doc["errorRankNum"],
-        "avgRectTime":0 
+        "avgRectTime": 0,
     }
     return res
-
