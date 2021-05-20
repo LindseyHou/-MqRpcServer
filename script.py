a03c05c9d82ff5c31add47613a7a0bb950867770
lrq619 <lrq619@sjtu.edu.cn>
2021-05-20 16:47:49 +0800

    [feat] fix bug 5.0

---
+++ script.py
@@ -0,0 +1,26 @@
+import os
+
+companyID_ints = [
+    107747,
+    107748,
+    116584,
+    130584,
+    130588,
+    130589,
+    130591,
+    130592,
+    130595,
+    130596,
+    135588,
+    137584,
+    139586,
+    144588,
+    145584,
+    149584,
+    153586,
+    168585,
+]
+
+for id in companyID_ints:
+    companyID = "CPYTEMP" + str(id)
+    os.system("python3 real_var.py " + companyID + ">" + companyID + ".log")
