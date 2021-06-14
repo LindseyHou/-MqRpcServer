4df07a6a4f873e85b5d89f34017026b937a6fb21
BoYanZh <bomingzh@sjtu.edu.cn>
2021-06-14 23:17:02 +0800

    fix: port 8091

--- rpc.py
+++ rpc.py
@@ -2,7 +2,7 @@ import uvicorn
 
 
 def main() -> None:
-    uvicorn.run("app:app", host="127.0.0.1", port=8001, reload=False)
+    uvicorn.run("app:app", host="127.0.0.1", port=8091, reload=False)
 
 
 if __name__ == "__main__":
